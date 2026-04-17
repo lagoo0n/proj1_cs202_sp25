1cf172dbd5bb686fbf6200705a04ab84471ed1af
lagoo0n <loganchook@gmail.com>
2026-04-17 13:16:54 -0700

    task 2 done

--- proj1.py
+++ proj1.py
@@ -17,3 +17,11 @@ class RegionCondition:
     year: int
     pop: int
     ghg_rate: float # tons of CO2 per year
+
+region_condition = [
+    RegionCondition(Region(GlobeRect(40.4, 41.4, -74.0, -73.0), "New York Metro", "other"), 2025, 20000000, 50000000.0),
+    RegionCondition(Region(GlobeRect(41.7, 42.1, 12.3, 12.7), "Rome Metro", "other"), 2025, 4000000, 10000000.0),
+    RegionCondition(Region(GlobeRect(5.0, 35.0, 120.0, 150.0), "Ring of Fire", "ocean"), 2025, 1000000, 1000000.0),
+    RegionCondition(Region(GlobeRect(35.0, 35.6, -120.9, -120.2), "San Luis Obispo", "other"), 2025, 300000, 500000.0)
+]
+
