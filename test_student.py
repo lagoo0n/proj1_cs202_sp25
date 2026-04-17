d0d41f17b959fe67f6db92708e01ddacd6764823
lagoo0n <loganchook@gmail.com>
2026-04-17 13:24:54 -0700

    added new test case

--- test_student.py
+++ test_student.py
@@ -12,9 +12,13 @@ class TestRegionFunctions(unittest.TestCase):
     def test_holder(self):
         pass
 
-    def test_emissions_per_capita(self):
+    def test1_emissions_per_capita(self):
         region = RegionCondition(Region(GlobeRect(35.0, 35.6, -120.9, -120.2), "San Luis Obispo", "other"), 2025, 300000, 500000.0)
         self.assertAlmostEqual(emissions_per_capita(region), 500000.0 / 300000)
+
+    def test2_emissions_per_capita(self):
+        region = RegionCondition(Region(GlobeRect(35.0, 35.6, -120.9, -120.2), "San Luis Obispo", "other"), 2025, 0, 100000000.0)
+        self.assertAlmostEqual(emissions_per_capita(region), 0.0)
 
 
 if __name__ == '__main__':
