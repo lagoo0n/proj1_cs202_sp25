e33ea025ea26ac8f629a4bbba6c6066f789130f1
lagoo0n <loganchook@gmail.com>
2026-04-16 17:18:34 -0700

    worked on task 1

--- proj1.py
+++ proj1.py
@@ -1,1 +1,20 @@
-#complete your tasks in this file
+from dataclasses import dataclass
+@dataclass(frozen=True)
+
+class GlobeRect:
+    lo_lat: float
+    hi_lat: float
+    west_long: float
+    east_long: float
+
+class Region:
+    rect: GlobeRect
+    name: str
+    terrain: str # e.g. "ocean", "mountain", "forest", "other"
+
+class RegionCondition:
+    region: Region
+    year: int
+    pop: int
+    ghg_rate: float # tons of CO2 per year
+
